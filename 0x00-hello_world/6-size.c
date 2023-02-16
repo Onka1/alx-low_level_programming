53219036770a65f592bc8c77d29dcd1f70cf1bb8
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-02-16 11:44:19 -0800

    yohh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %i byte(s)\n", sizzeof(char));
-	printf("Size of an int: %i byte(s)\n", sizzeof(int));
-	printf("Size of a long int: %i byte(s)\n", sizzeof(long int));
-	printf("Size of a long long int: %i byte(s)\n", sizzeof(long long int));
-	printf("Size of a float: %i byte(s)\n", sizzeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizzeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizzeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizzeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizzeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizzeof(float));
 	return (0);
 }
