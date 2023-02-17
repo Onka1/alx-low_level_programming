77a8cb650eb32fe3f1508ebcb5cb342056b81f39
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-02-17 09:44:47 -0800

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdli.h>
+/**
+ * main - main block
+ * Description : Alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char c + 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
