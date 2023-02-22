373293e12cb1890f73e7e77cf061bd5f6bf5d54d
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-02-21 20:58:06 -0800

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - a function that checks for
+ * alphabetic character
+ * Return: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
