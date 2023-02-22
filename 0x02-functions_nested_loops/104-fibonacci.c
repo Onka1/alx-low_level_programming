8e72c77c07b08b5584250cbc94e99cc8784c1558
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-02-22 15:26:24 -0800

    task16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - a program that finds and prints
+ * the first 98 Fibonacci numbers, starting
+ * with 1 and 2, followed by a new line.
+ * Return: required
+ */
+int main(void)
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
