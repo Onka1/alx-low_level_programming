ac7717e27b8378df34fe84f51222551c747c4c36
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-02-22 13:49:27 -0800

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * add - a function that adds two integers
+ * and returns the result.
+ * Return: expected output
+ */
+int add(int int1, int int2)
+{
+	return (int1 + int2);
+}
