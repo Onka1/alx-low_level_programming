232c0f2bcc79b69384b7c170990a67bda2e6f6ea
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-02-27 12:17:52 -0800

    task1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * swap_int -  a function that swaps the values
+ * of two integers.
+ * Return: expected
+ */
+void swap_int(int *a, int *b)
+{
+	int now = *a;
+	*a = *b;
+	*b = temp;
+}
