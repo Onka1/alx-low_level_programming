ead546322381d4e4711991a3535d041c1f400cc7
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-02-27 12:08:48 -0800

    poiny

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,17 +1,9 @@
-#include "main.h"
 #include <stdio.h>
 /**
- * main - check code
- *
- * Return: 0
+ * reset_to_98 - pointer to int to 98
+ * Return: expected;
  */
-int main(void)
+void reset_to_98(int *n)
 {
-	int n;
-
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+	*n = 98;
 }
