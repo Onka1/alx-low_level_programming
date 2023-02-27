baf8ad208ed23d0d47eb833866f10fe8dc3cd36a
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-02-27 13:20:06 -0800

    task5

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - function that prints a string, followed by a
+ * new line, to stdout
+ * Return: expected
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
