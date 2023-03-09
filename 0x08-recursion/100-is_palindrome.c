354a8a6e992b425abeee34eccba495152014c766
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-03-09 11:08:31 -0800

    task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,13 +1,40 @@
 #include "main.h"
 
 /**
- * is_palindrome - ..
+ * _strlen_recursion - ..
  * Return: expected
  */
-int is_palindrome(char *s)
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * comparator - ..
+ * Return: .
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - is string a ..
+ * Return: 1,0
+ *
+ */
+int  is_palindrome(char *s)
 {
 	if (*s == '\0')
 		return (1);
-	else
-		return (1 + is_palindrome(s + 1));
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
 }
