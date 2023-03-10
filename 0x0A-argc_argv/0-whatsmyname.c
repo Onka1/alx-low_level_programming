74def5131fe5a7878279d79ca9363b3612e2f162
Onka1 <122876282+Onka1@users.noreply.github.com>
2023-03-10 14:00:40 -0800

    there

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * Return: 0
+ */
+int main (int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
